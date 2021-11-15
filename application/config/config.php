348977a64c3d9f032e340803cedc7896921de6c2
mashan-lomastxt <riska.handika@students.amikom.ac.id>
2021-11-15 09:52:50 +0700

    change port url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/CI-PCS/';
+$config['base_url'] = 'http://localhost/CI-PCS/';
 
 /*
 |--------------------------------------------------------------------------
